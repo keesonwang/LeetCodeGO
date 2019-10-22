3e92e044bbcf28ec30eda2d16d0a74ef96de0ff4
keesonwang <qisenwang@yahoo.ca>
2019-10-22 15:48:18 -0400

    Update 9. PalindromeNumber.js
    !! reverse without new array or string

--- 9. PalindromeNumber.js
+++ 9. PalindromeNumber.js
@@ -21,3 +21,11 @@ var isPalindrome = function(x) {
 
 
 //not convert to string
+//!! reverse without new array or string
+var isPalindrome = function(x) {
+  if (x < 0) return false
+
+  let rev = 0
+  for(let i = x; i >= 1; i = Math.floor(i/10)) rev = rev*10 + i%10
+  return rev === x
+};
