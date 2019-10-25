948552d1e558e7634e402395a033911d2b206a77
keesonwang <qisenwang@yahoo.ca>
2019-10-24 21:25:23 -0400

    Create 35. Search Insert Position.js

---
+++ 35. Search Insert Position.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0, right = nums.length-1;
+    if (target<nums[left]) return 0;
+    if (target>nums[right]) return right+1;
+    while(left<=right){
+        let mid = Math.floor((left + right)/2);
+        if (nums[mid]===target) return mid;
+        else if (nums[mid] < target)  
+             left = mid + 1; 
+        else
+             right = mid - 1; 
+     } 
+    return left
+};
