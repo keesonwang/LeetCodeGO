bdd70d20b9568ddbe78be1c1200f743aa601b626
keesonwang <qisenwang@yahoo.ca>
2019-11-01 01:15:49 -0400

    Create 80. Remove Duplicates from Sorted Array II.js

---
+++ 80. Remove Duplicates from Sorted Array II.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {    
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] !== nums[j]) break;
+            
+            if (j - i > 1) {
+                nums.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    
+    return nums.length;
+};
